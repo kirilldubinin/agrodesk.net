e10199cf91f3d32ec611d11d1e900abf94c1a311
kirilldubinin <dubininkg@gmail.com>
2020-01-12 17:28:55 +0300

    master

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-var port = process.env.PORT || 8081;
+var port = process.env.PORT || 8080;
 var database = require('./config/database');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
